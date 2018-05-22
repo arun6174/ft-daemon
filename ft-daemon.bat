3870e229bcc3020d46217b62dee1e06d1765594a
arun6174 <hindol96@gmail.com>
2018-05-22 21:18:34 +0800

    Updated shell and batch script to start/stop daemon

--- ft-daemon.bat
+++ ft-daemon.bat
@@ -1,75 +1,36 @@
 @echo off
+
 where /q python
-if ERRORLEVEL 1 (
-    @echo Python 2.7 is not installed im the system.
-    @echo.
-    @echo Download Python 2.7 from here - https://www.python.org/ftp/python/2.7.15/python-2.7.15.msi and install.
-) else (
-    @echo on
-    python src/main.py
-)
-@pause
-exit /B
-
-
-
-::::::::::::::::::::::::::::::::::::::::::::
-:: Elevate.cmd - Version 4
-:: Automatically check & get admin rights
-::::::::::::::::::::::::::::::::::::::::::::
- @echo off
- CLS
- ECHO.
- ECHO =============================
- ECHO Running Admin shell
- ECHO =============================
-
-:init
-    setlocal DisableDelayedExpansion
-    set cmdInvoke=1
-    set winSysFolder=System32
-    set "batchPath=%~0"
-    for %%k in (%0) do set batchName=%%~nk
-    set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
-    setlocal EnableDelayedExpansion
-
-:checkPrivileges
-    NET FILE 1>NUL 2>NUL
-    if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
-
-:getPrivileges
-    if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
-    ECHO.
-    ECHO **************************************
-    ECHO Invoking UAC for Privilege Escalation
-    ECHO **************************************
-
-    ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
-    ECHO args = "ELEV " >> "%vbsGetPrivileges%"
-    ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
-    ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
-    ECHO Next >> "%vbsGetPrivileges%"
-
-    if '%cmdInvoke%'=='1' goto InvokeCmd
-
-    ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
-    goto ExecElevation
-
-:InvokeCmd
-    ECHO args = "/c """ + "!batchPath!" + """ " + args >> "%vbsGetPrivileges%"
-    ECHO UAC.ShellExecute "%SystemRoot%\%winSysFolder%\cmd.exe", args, "", "runas", 1 >> "%vbsGetPrivileges%"
-
-:ExecElevation
-    "%SystemRoot%\%winSysFolder%\WScript.exe" "%vbsGetPrivileges%" %*
+if '%errorlevel%' == '1' (
+    echo.
+    echo Python 2.7 is not installed im the system.
+    echo Download Python 2.7 from here - https://www.python.org/ftp/python/2.7.15/python-2.7.15.msi and install.
+    echo.
+    @pause
     exit /B
+)
 
-:gotPrivileges
-    setlocal & cd /d %~dp0
-    if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
+:: Get parameters
+set action=none
+if "%1" == "start" set action=%1
+if "%1" == "stop" set action=%1
+if "%action%" == "none" (
+    echo.
+    echo Invalid parameter or parameter missing^!
+    echo Usage: %0 ^<start^/stop^>
+    echo.
+    @pause
+    exit /B
+)
 
-    ::::::::::::::::::::::::::::
-    ::START
-    ::::::::::::::::::::::::::::
-    REM Run shell as admin (example) - put here code as you like
-    ECHO %batchName% Arguments: P1=%1 P2=%2 P3=%3 P4=%4 P5=%5 P6=%6 P7=%7 P8=%8 P9=%9
-    cmd /k
+NET FILE 1>NUL 2>NUL
+if not '%errorlevel%' == '0' (
+    echo.
+    echo You need to run this script as administrator.
+    echo Right-click on 'Command Prompt' icon in Start menu and click 'Run as administrator'.
+    echo.
+    @pause
+    exit /B
+)
+
+python src/main.py "%action%"
--- ft-daemon.sh
+++ ft-daemon.sh
@@ -1,1 +1,16 @@
-python2.7 src/main.py
+#!/bin/bash
+action=$1
+if [ "$action" != "start" -a "$action" != "stop" ]; then
+    echo "Usage: $0 <start/stop>"
+    exit 1
+fi
+
+[ -z "$(which python2.7)" ] && {
+    echo "Python 2.7 is not installed im the system."
+    echo "Install Python 2.7 and try again."
+    exit 1
+}
+
+my_dir="`dirname "$0"`"
+echo ""
+python2.7 $my_dir/src/main.py "$1"
